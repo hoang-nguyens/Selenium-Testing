b6150335c937cea5d998bcae751a455615948b94
unknown <hoang0403ss@gmail.com>
2024-10-20 10:36:26 +0700

    save link to csv file

--- src/test/java/seleniumfirst/RunSelenium.java
+++ src/test/java/seleniumfirst/RunSelenium.java
@@ -9,8 +9,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.opencsv.CSVWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RunSelenium {
 	public static void main(String[] args) {
@@ -40,9 +49,9 @@ public class RunSelenium {
 		WebElement searchField = wait.until(ExpectedConditions.visibilityOfElementLocated(
 				By.cssSelector("[placeholder='Search']")));
 	    searchField.sendKeys("#blockchain");
+	    searchField.submit();
 	      
-	    Actions act = new Actions(driver);
-	    act.keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
+//	    Actions act = new Actions(driver);
 	    
 	    WebElement people = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.cssSelector("a[href='/search?q=%23blockchain&src=typed_query&f=user']")
@@ -50,28 +59,72 @@ public class RunSelenium {
 	    people.click();
 	    
 	    try {
-            Thread.sleep(5000);  // Pause for 5000 milliseconds (5 seconds) Pause more for page loading and gain more data
+            Thread.sleep(5000);  
         } catch (InterruptedException e) {
-            e.printStackTrace();  // Handle the interrupted exception
+            e.printStackTrace();  
         }
 	    
-	    List<WebElement> users = driver.findElements(By.xpath("//div[@data-testid='cellInnerDiv']"));
+//	    List<WebElement> users = driver.findElements(By.xpath("//div[@data-testid='cellInnerDiv']"));
+//	    
+//	    int size = users.size();
+//	    System.out.println(size);
+//	    
+//	    for (WebElement user: users) {
+//	    	String profileLink = user.findElement(By.tagName("a")).getAttribute("href");
+//	    	System.out.println(profileLink);
+//	    	
+//	    	String displayname = user.findElement(By.xpath(".//div[contains(@dir, 'ltr')]/span")).getText();
+//	    	System.out.println(displayname);
+//	    	
+//	    	String username = user.findElement(By.xpath(".//a[contains(@href, '/')]//span[contains(text(), '@')]")).getText();
+//	    	System.out.println(username + '\n');
+//	    }
 	    
-	    int size = users.size();
-	    System.out.println(size);
-	    
-	    for (WebElement user: users) {
-	    	String profileLink = user.findElement(By.tagName("a")).getAttribute("href");
-	    	System.out.println(profileLink);
-	    	
-	    	String displayname = user.findElement(By.xpath(".//div[contains(@dir, 'ltr')]/span")).getText();
-	    	System.out.println(displayname + '\n');
-	    	
-	    	String username = user.findElement(By.xpath(".//a[contains(@href, '/')]//span[contains(text(), '@')]")).getText();
-	    	System.out.println(username + '\n');
+	    Actions act = new Actions(driver);
+	    WebElement user;
+
+	    List<WebElement> users = driver.findElements(By.cssSelector("[data-testid='UserCell'][role='button']"));
+	    Set<String> listLink = new LinkedHashSet<>();
+
+	    for (int i = 0; i < 30; i++) {
+
+	        try {
+	            Thread.sleep(2000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+
+	        users = driver.findElements(By.cssSelector("[data-testid='UserCell'][role='button']"));
+
+	        if (i >= users.size()) {
+	            user = users.get(users.size() - 1);
+	        } else {
+	            user = users.get(i);
+	        }
+
+	        user.sendKeys(Keys.ARROW_DOWN);
+
+	        String href = user.findElement(By.tagName("a")).getAttribute("href");
+	        listLink.add(href);      
+	        System.out.println(href);
+	        
 	    }
+	    
+	    String fileName = "output.csv";
+
+        
+        try (CSVWriter writer = new CSVWriter(new FileWriter(fileName, true))) { // Set to true for append mode
+            for (String element : listLink) {
+                String[] record = { element };
+                writer.writeNext(record); // Write the record to the CSV
+            }
+            System.out.println("Set appended to " + fileName);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+	    	
 	    
 	    
 	}
-
 }
+
